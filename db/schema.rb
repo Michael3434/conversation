f86a3ef773123c9b37c5c73bae9e0444b69c0820
Michael monin <mt.monin@gmail.com>
2015-11-28 01:09:09 +0100

    Create photo model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151124171511) do
+ActiveRecord::Schema.define(version: 20151128000803) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "topic"
@@ -32,6 +32,18 @@ ActiveRecord::Schema.define(version: 20151124171511) do
 
   add_index "messages", ["ancestry"], name: "index_messages_on_ancestry"
   add_index "messages", ["sent_messageable_id", "received_messageable_id"], name: "acts_as_messageable_ids"
+
+  create_table "photos", force: :cascade do |t|
+    t.integer  "message_id"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+  end
+
+  add_index "photos", ["message_id"], name: "index_photos_on_message_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
